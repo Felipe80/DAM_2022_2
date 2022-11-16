0b10106d45e2d2e2278f9cd1f9dfd3fd49cd2a65
Felipe80 <felipealten@gmail.com>
2022-11-16 16:14:52 -0300

    modelos con relaciones y campos calculados

--- U3/ejercicio_venta_productos/venta_productos_api/app/Models/Producto.php
+++ U3/ejercicio_venta_productos/venta_productos_api/app/Models/Producto.php
@@ -14,10 +14,26 @@ class Producto extends Model
     protected $primaryKey = 'cod_producto';
     protected $keyType = 'string';
     public $timestamps = false;
+    protected $appends = ['cantidad_ventas','unidades_vendidas'];
 
     //retornar las ventas en que está un producto
     public function ventas(){
         //modelo al que apunta,nombre tabla pivote
         return $this->belongsToMany(Venta::class,'producto_venta','cod_producto','venta_id');
+    }
+
+    public function ventasConPivot(){
+        //modelo al que apunta,nombre tabla pivote
+        return $this->belongsToMany(Venta::class,'producto_venta','cod_producto','venta_id')->withPivot('cantidad','precio_venta');
+    }
+
+    //cantidad de ventas en que está un producto
+    public function getCantidadVentasAttribute(){
+        return count($this->ventas);
+    }
+
+    //cantidad de unidades vendidas del producto
+    public function getUnidadesVendidasAttribute(){
+        return $this->ventasConPivot->sum('pivot.cantidad');
     }
 }
--- U3/ejercicio_venta_productos/venta_productos_api/app/Models/Venta.php
+++ U3/ejercicio_venta_productos/venta_productos_api/app/Models/Venta.php
@@ -13,9 +13,23 @@ class Venta extends Model
     protected $table = 'ventas';
     protected $keyType = 'integer';
     public $timestamps = false;
+    protected $appends = ['total_venta'];
 
     //retorna los productos de la venta
     public function productos(){
         return $this->belongsToMany(Producto::class,'producto_venta','venta_id','cod_producto');
+    }
+
+    public function productosConPivot(){
+        return $this->belongsToMany(Producto::class,'producto_venta','venta_id','cod_producto')->withPivot('cantidad','precio_venta');
+    }
+
+    //indicar el total de la venta
+    public function getTotalVentaAttribute(){
+        $totalVenta = 0;
+        foreach($this->productosConPivot as $prod){
+            $totalVenta += $prod->pivot->cantidad * $prod->pivot->precio_venta;
+        }
+        return $totalVenta;
     }
 }
